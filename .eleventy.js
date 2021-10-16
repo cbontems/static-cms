feacca571cedfdf00bce3c33e9759dfaebbe845b
cbontems <67456035+cbontems@users.noreply.github.com>
2021-10-16 22:39:19 +0200

    Date locale to FR

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/admin');
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
     eleventyConfig.addFilter("postDate", (Dateobj) => {
-      return DateTime.fromJSDate(Dateobj).toLocaleString(DateTime.DATE_MED);
+      return DateTime.fromJSDate(Dateobj).setLocale('fr').toLocaleString(DateTime.DATE_FULL);
     });
 
     return {
